97fcbedf5eee45429f42dc90dafbd137a82908fa
Enas Abuiweimer <enas@gmail.com>
2023-10-19 11:49:51 +0300

    hover-still

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// const cardContent = document.querySelector(".card-content");
+// const showLink = document.querySelector(".showlinks");
+// cardContent.addEventListener('hover', function(){
+//     showLink.style.display = "block";
+// });
