5e734ba1e6ef84fdcc87745f4aa888a0ff4a683d
enas-kh <enkhowimer@gmail.com>
2023-10-20 00:05:38 +0300

    hover-added

--- assets/script.js
+++
@@ -1,5 +0,0 @@
-// const cardContent = document.querySelector(".card-content");
-// const showLink = document.querySelector(".showlinks");
-// cardContent.addEventListener('hover', function(){
-//     showLink.style.display = "block";
-// });
